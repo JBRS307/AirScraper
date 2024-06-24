9939ee0fbdd4483080247f1dae337137f28b84cf
JBRS307 <kuba.rekas.307@gmail.com>
2024-06-24 22:17:33 +0200

    added __str__ functions to Station and Installation classes

--- installation.py
+++ installation.py
@@ -1,5 +1,8 @@
 class Installation:
-    def __init__(self, id, station_id, param_code):
+    def __init__(self, id: int, station_id: int, param_code: str):
         self.id = id
         self.station_id = station_id
         self.param_code = param_code
+    
+    def __str__(self):
+        return f"installation #{self.id}: '{self.param_code}'"
--- station.py
+++ station.py
@@ -1,5 +1,12 @@
+from installation import Installation
+
 class Station:
-    def __init__(self, id, name, installations = []):
+    def __init__(self, id: int, name: str, installations: list[Installation] = []):
         self.id = id
         self.name = name
         self.installations = installations
+    
+    def __str__(self) -> str:
+        header = f"Station #{self.id} ({self.name}):\n"
+        installations_str = "\n".join(self.installations)
+        return header + installations_str

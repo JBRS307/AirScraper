12ed7edf1a7ac5282adbaddf402dff4e4e4463b2
JBRS307 <kuba.rekas.307@gmail.com>
2024-06-24 20:53:16 +0200

    classes for installation and station created

---
+++ installation.py
@@ -0,0 +1,5 @@
+class Installation:
+    def __init__(self, id, station_id, param_code):
+        self.id = id
+        self.station_id = station_id
+        self.param_code = param_code
---
+++ station.py
@@ -0,0 +1,5 @@
+class Station:
+    def __init__(self, id, name, installations = []):
+        self.id = id
+        self.name = name
+        self.installations = installations

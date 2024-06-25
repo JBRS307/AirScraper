ae3632cab6350992ba958d87cc7a778bb32c51b5
JBRS307 <kuba.rekas.307@gmail.com>
2024-06-25 15:32:19 +0200

    Modified station class, so the list is not common for every object (fixed)

--- station.py
+++ station.py
@@ -2,12 +2,17 @@ from installation import Installation
 
 # Class to store required information about each station
 class Station:
-    def __init__(self, id: int, name: str, installations: list[Installation] = []):
+    def __init__(self, id: int, name: str, installations: list[Installation] = None):
         self.id = id
         self.name = name
+        if installations is None:
+            installations = []
         self.installations = installations
     
     def __str__(self) -> str:
         header = f"Station #{self.id} ({self.name}):\n"
-        installations_str = "\n".join(self.installations)
+        installations_str = "\n".join([str(installation) for installation in self.installations])
         return header + installations_str
+    
+    def add_installation(self, installation: Installation):
+        self.installations.append(installation)

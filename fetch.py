d6ff4004e4f8c51b820c1d35615b8d8bafeb5f5e
JBRS307 <kuba.rekas.307@gmail.com>
2024-06-25 14:29:02 +0200

    REFACTOR: fetch_url moved to separate file fetch.py, changed constants names in fetch_stations

---
+++ fetch.py
@@ -0,0 +1,19 @@
+import requests as rq
+from time import sleep
+
+# Wrapper function for fetching.
+def fetch_url(url) -> rq.Response:
+    while True:
+        res = rq.get(url)
+
+        # Response status codes that we need to care about are 200 (Success) and
+        # 429 (Too many requests), if any other code occurs we raise an exception
+        if res.status_code != 200 and res.status_code != 429:
+            raise rq.RequestException("Request failed with code: " + str(res.status_code))
+        
+        # In case of too frequent requests we're gonna have to wait some time
+        if res.status_code == 429:
+            print("Too many requests, waiting 30 seconds for next request...")
+            sleep(30)
+        else:
+            return res
--- fetch_stations.py
+++ fetch_stations.py
@@ -1,51 +1,34 @@
 import requests as rq
 from station import Station
-from time import sleep
+from fetch import fetch_url
 
 # Stores maximum page size available at api request (we want big pages
 # because we want to fetch info about all stations
 PAGE_SIZE = 500
 
 # Base URL for stations API
-STATIONS_URL = "https://api.gios.gov.pl/pjp-api/v1/rest/station/findAll"
+URL = "https://api.gios.gov.pl/pjp-api/v1/rest/station/findAll"
 
-STATIONS_ARG = "Lista stacji pomiarowych" # JSON key for station list
-STATION_ID = "Identyfikator stacji" # JSON key for station ID
-STATION_NAME = "Nazwa stacji" # JSON key for station name
+ARG = "Lista stacji pomiarowych" # JSON key for station list
+ID = "Identyfikator stacji" # JSON key for station ID
+NAME = "Nazwa stacji" # JSON key for station name
 
 # Function to modify base url to get maximum number of stations
 # on one page and to change the page
 def set_stations_url(page: int, size: int) -> str:
-    global STATIONS_URL
+    global URL
 
     if page < 0:
         raise ValueError("Page number cannot be nagative!")
     if size < 0:
         raise ValueError("Page size cannot be negative!")
     
-    return STATIONS_URL + "?page=" + str(page) + "&size=" + str(size)
+    return URL + "?page=" + str(page) + "&size=" + str(size)
 
-
-# Wrapper function for fetching.
-def fetch_url(url) -> rq.Response:
-    while True:
-        res = rq.get(url)
-
-        # Response status codes that we need to care about are 200 (Success) and
-        # 429 (Too many requests), if any other code occurs we raise an exception
-        if res.status_code != 200 and res.status_code != 429:
-            raise rq.RequestException("Request failed with code: " + str(res.status_code))
-        
-        # In case of too frequent requests we're gonna have to wait some time
-        if res.status_code == 429:
-            print("Too many requests, waiting 30 seconds for next request...")
-            sleep(30)
-        else:
-            return res
 
 # Main fetch function for stations
 def fetch_stations() -> list[Station]:
-    global PAGE_SIZE, STATIONS_ARG, STATION_ID, STATION_NAME
+    global PAGE_SIZE, ARG, ID, NAME
 
     url = set_stations_url(0, PAGE_SIZE)
 
@@ -53,8 +36,8 @@ def fetch_stations() -> list[Station]:
     
     stations = []
     res_json = res.json() # Variable to avoid multiple calls
-    for station in res_json[STATIONS_ARG]:
-        stations.append(Station(int(station[STATION_ID]), station[STATION_NAME]))
+    for station in res_json[ARG]:
+        stations.append(Station(int(station[ID]), station[NAME]))
     
     pages = int(res_json["totalPages"])
     
@@ -62,8 +45,8 @@ def fetch_stations() -> list[Station]:
     for i in range(1, pages):
         url = set_stations_url(i, PAGE_SIZE)
         res = fetch_url(url)
-        for station in res.json()[STATIONS_ARG]:
-            stations.append(Station(int(station[STATION_ID]), station(STATION_NAME)))
+        for station in res.json()[ARG]:
+            stations.append(Station(int(station[ID]), station(NAME)))
     
     return stations
 
